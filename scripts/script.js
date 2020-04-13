4364c0d6d2cbec0df5a11ee2b0500067b19b19f0
Artem Varlamov <svre1337@yandex.ru>
2020-04-13 18:50:09 +0300

    mesto v.2

--- scripts/script.js
+++ scripts/script.js
@@ -2,39 +2,31 @@ const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close');
 const submitButton = document.querySelector('.popup__submit');
-
+const nameInput = document.querySelector('.popup__name');
+const aboutInput = document.querySelector('.popup__about');
+const profileTitle = document.querySelector('.profile__info-title');
+const profileSubtitle = document.querySelector('.profile__info-subtitle');
+const formElement = document.querySelector('.popup__form');
 
 function popupOpen() {
-    
-    popup.classList.remove('popup');
+    document.getElementsByClassName('popup');
     popup.classList.add('popup_opened');
+    nameInput.value = profileTitle.textContent;
+    aboutInput.value = profileSubtitle.textContent;
 }
-
-editButton.addEventListener('click', popupOpen);
 
 function popupClose() {
-    
+    document.getElementsByClassName('popup popup_opened');
     popup.classList.remove('popup_opened');
-    popup.classList.add('popup');
 }
-
-popupCloseButton.addEventListener('click', popupClose);
-
-const formElement = document.querySelector('.popup__form');
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-  
-    const nameInput = document.querySelector('.popup__name');
-    const aboutInput = document.querySelector('.popup__about');
-    const profileTitle = document.querySelector('.profile__info-title');
-    const profileSubtitle = document.querySelector('.profile__info-subtitle');
-
-    nameInput.value;
-    aboutInput.value;
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = aboutInput.value;
 }
 
+editButton.addEventListener('click', popupOpen);
+popupCloseButton.addEventListener('click', popupClose);
 popup.addEventListener('submit', formSubmitHandler);
 submitButton.addEventListener('click', popupClose);
